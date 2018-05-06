cd72778fe852d0db5a98726636f155749431f6f6
steincodes <swarnimarun11@gmail.com>
2018-05-06 20:06:23 +0530

    Adding comments and notes
    Added comments and notes to help with understanding the code and learn about creating proper Custom Nodes.

--- addons/vs_custom_nodes/GetInputDirection2D.gd
+++ addons/vs_custom_nodes/GetInputDirection2D.gd
@@ -1,36 +1,53 @@
+# Make sure to use tool keyword as the code needs to run in the editor
 tool
 extends VisualScriptCustomNode
 
-func _get_caption():
-	return "GetInputDirection2D"
+func _get_caption(): # the name of the custom node [Make sure to give them longer names to be able to find them with ease].
+	return "Get Input Direction 2D"
 
-func _get_category():
+func _get_category(): # the category of the custom node
 	return "Input"
 	
-func _get_text():
-	return "Input"
+func _get_text(): # the text displayed after input sequence arrow/port
+	return ""
 	
-func _get_input_value_port_count():
+func _get_input_value_port_count(): # number of inputs to be taken
 	return 0
+
+func _get_input_value_port_type(idx): # type of the inputs per index starting from 0
+	return TYPE_OBJECT
 	
-func _get_output_value_port_count():
+func _get_output_value_port_count(): # number of outputs to be given
 	return 1
 	
-func _get_output_value_port_type(idx):
+func _get_output_value_port_type(idx): # type of outputs per index starting from 0
 	return TYPE_VECTOR2
 	
-func _get_output_value_port_name(idx):
+func _get_output_value_port_name(idx): # name/text displayed before output node per index
 	return "Direction"
 
-func _has_input_sequence_port():
+func _has_input_sequence_port(): # if the node requires sequence port
 	return true
 	
-func _get_output_sequence_port_count():
+func _get_output_sequence_port_count(): # number of output sequence ports to use(have at least one if you have an input sequence port)
 	return 1
 	
-func _step(inputs, outputs, start_mode, working_mem):
+func _step(inputs, outputs, start_mode, working_mem): # the primary method where all the working takes place
+	
+	# inputs array is used as data and can be accessed as input[0] and so on.....
+	
 	var x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	var y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	# output array is used to give out output, and you only have to give the value
+	                 # to the specific index of outputs to give out output of any kind
 	outputs[0] = Vector2(x,y)
-	return 0
+	
+	# you can use start_mode to check and see if you have to perform the operation again and again or not
+	# i.e. to say that you can use the _step() as ready when the start_modes value tells that
+	# it's the first iteration or as process...........
+	
+	# and finally working memory can be used to check the working memory and it's size. Read more about it in the godot-docs.
+	
+	return 0 # return the error string for errors else 0
 	
--- addons/vs_custom_nodes/addvsnodes.gd
+++ addons/vs_custom_nodes/addvsnodes.gd
@@ -1,24 +1,14 @@
 tool
 extends EditorPlugin
 
-#const vsnode1 = preload("res://addons/vs_custom_nodes/node1.gd") # place holders for future nodes
-#const vsnode2 = preload("res://addons/vs_custom_nodes/node2.gd")
-#const vsnode3 = preload("res://addons/vs_custom_nodes/node3.gd")
-#actual test script
+# getting the script for custom node
 const getinput = preload("res://addons/vs_custom_nodes/GetInputDirection2D.gd")
 
 func _enter_tree():
+	# add the custom node to the Visual Script Editor on start-up
 	VisualScriptEditor.add_custom_node("GetInputDirection2D", "Input", getinput)
-#	if vsnode1 == null || vsnode2 == null || vsnode3 == null:
-#		return
-#	VisualScriptEditor.add_custom_node("Node 1", "Extensions", vsnode1)
-#	VisualScriptEditor.add_custom_node("Node 2", "Extensions", vsnode2)
-#	VisualScriptEditor.add_custom_node("Node 3", "Extensions", vsnode3)
+	# the add_custom_node() takes the "name" and "category" as index that will be used to later remove the node
 	
 func _exit_tree():
+	# remove the custom node to the Visual Script Editor on exit
 	VisualScriptEditor.remove_custom_node("GetInputDirection2D", "Input")
-#	if vsnode1 == null || vsnode2 == null || vsnode3 == null:
-#		return
-#	VisualScriptEditor.remove_custom_node("Node 1", "Extensions")
-#	VisualScriptEditor.remove_custom_node("Node 2", "Extensions")
-#	VisualScriptEditor.remove_custom_node("Node 3", "Extensions")
